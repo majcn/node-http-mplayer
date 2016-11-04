490b96900cc4165d331485c03c1245f15ba116a7
Gregor Majcen <majcn.m@gmail.com>
2016-11-04 12:31:24 +0100

    React.Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Template from './template/Template'
 
 import StationCard from './components/StationCard'
 
-class App extends Component {
+class App extends React.Component {
     render() {
         let stations = [1,1,1,1,1,1,1,1].map(station => (
            <StationCard />
