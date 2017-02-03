50985e5e256b6e235458357ef70a23b23d4d17ab
Gregor Majcen <majcn.m@gmail.com>
2017-02-03 18:57:05 +0100

    next

--- routes/controls.js
+++ routes/controls.js
@@ -9,4 +9,10 @@ router.get('/volume/:value(1?[0-9]?[0-9])', (req, res, next) => {
   res.json(volume);
 });
 
+router.get('/next', (req, res, next) => {
+  req.malinca.player.next();
+
+  res.json("next");
+});
+
 module.exports = router;
