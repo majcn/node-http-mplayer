76bb65ce59ab91cb64ad4bb437468d8815c56a68
Gregor Majcen <majcn.m@gmail.com>
2017-02-03 18:44:51 +0100

    typo

--- routes/controls.js
+++ routes/controls.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/volume/:value([1?[0-9]?[0-9])', (req, res, next) => {
+router.get('/volume/:value(1?[0-9]?[0-9])', (req, res, next) => {
   let volume = req.params.value;
 
   req.malinca.player.volume(volume);
